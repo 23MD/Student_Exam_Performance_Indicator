a01f141a3a27ae3a81dd41269971ffe68549ec94
Mihir Damania <mihir.damania@gmail.com>
2024-05-31 17:25:09 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 #to perfrom data ingestion there need an input for example like path for storing raw data, trained data etc
 # therefore we will make a class which we can use for all input
@@ -69,4 +72,7 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     #train_arr,test_arr=
-    data_transformation.initiate_data_tranformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_tranformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
